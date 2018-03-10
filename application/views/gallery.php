88e568c21ca8e1a5dfec6c89350c61a36654d8f1
Dr. Naveen Tewari <navtewari@gmail.com>
2018-03-10 21:00:51 +0530

    up
    up

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -3,8 +3,12 @@
 </div>
 <!---->
 <div class="gallery">
-    <div class="container">
-        <h2>Gallery</h2>
+    <div class="container">        
+        <?php foreach ($gallery as $item) { ?>
+        <h2><?php echo $item->CATEGORY;?>-Gallery</h2>
+        <div style="float: right; margin-bottom: 2em;"><a href="<?php echo site_url('web/gallery')?>"  style="color:#ff6600"><i class="fa fa-undo" aria-hidden="true"></i> Back to Gallery</a></div>
+        <div style="clear:both;"></div>
+        <?php break; } ?> 
         <link rel="stylesheet" href="<?php echo base_url('assets/css/swipebox.css');?>">
         <script src="<?php echo base_url('assets/js/jquery.swipebox.min.js');?>"></script> 
         <script type="text/javascript">
@@ -13,9 +17,9 @@
             });
         </script>
         <div class="event-pics">
-            <?php for($loop=1;$loop<=4;$loop++){?>
-            <a href="<?php echo base_url('assets/images/'.$loop.'.jpg');?>" class="b-link-stripe b-animate-go  swipebox"  title="THe Jai Durge Educational Public School">
-                <img src="<?php echo base_url('assets/images/'.$loop.'.jpg');?>" alt="" class="img-responsive">
+            <?php foreach ($gallery as $item) { ?> 
+            <a href="<?php echo ADMIN___ . '/_assets_/gallery/' . $item->PHOTO_; ?>" class="b-link-stripe b-animate-go  swipebox"  title="THe Jai Durge Educational Public School">
+                <img src="<?php echo ADMIN___ . '/_assets_/gallery/' . $item->PHOTO_; ?>" alt="" class="img-responsive">
             </a>            
             <?php }?>
             <div class="clearfix"></div>
